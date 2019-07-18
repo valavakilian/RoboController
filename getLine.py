85c18ea07e2afed6609ea764bc847bf4f0d89c43
zack watkins <zackwatkins604@gmail.com>
2019-07-18 14:13:51 -0700

    change getLine.py to write to roboson file

--- getLine.py
+++ getLine.py
@@ -16,38 +16,47 @@ import argparse
 import imutils
 import time
 
+from RoboLoader import loadRobot, dumpRobot
 
-camera = PiCamera()
-camera.resolution = (40, 32)
-#camera.framerate = 32
-rawCapture = PiRGBArray(camera, size = (40, 32))
-camera.capture(rawCapture, format = 'bgr')
-frame = rawCapture.array
-rawCapture.truncate(0)
-kernel = np.array([[0,1/3,0]*3])
+def updateLine(filename = 'ROBOSON.json'):
+        robot = loadRobot(filename)
 
-for frame in camera.capture_continuous(rawCapture, format = "bgr", use_video_port = True):
-    frame = frame.array
-    frame_copy = frame.copy()
-    gray = cv2.cvtColor(frame_copy, cv2.COLOR_BGR2GRAY)
-    kerneled_image = cv2.filter2D(gray, -1, kernel)
-    ret, binary = cv2.threshold(kerneled_image, 50, 255, cv2.THRESH_BINARY)
-    captureBin = binary
-    capture = frame_copy
-    cv2.imshow('Binary', binary)
-    rawCapture.truncate(0)
-    if (cv2.waitKey(33)) == ord('a'):
-        break
-    
-    
-    
+        camera = PiCamera()
+        camera.resolution = robot.line_finder.resolution
+        #camera.framerate = 32
+        rawCapture = PiRGBArray(camera, size = robot.line_finder.resolution)
+        camera.capture(rawCapture, format = 'bgr')
+        frame = rawCapture.array
+        rawCapture.truncate(0)
+        kernel = np.array(robot.line_finder.kernel)
+
+        for frame in camera.capture_continuous(rawCapture, format = "bgr", use_video_port = True):
+        frame = frame.array
+        frame_copy = frame.copy()
+        gray = cv2.cvtColor(frame_copy, cv2.COLOR_BGR2GRAY)
+        kerneled_image = cv2.filter2D(gray, -1, kernel)
+        ret, binary = cv2.threshold(kerneled_image, robot.line_finder.binary_cut, 255, cv2.THRESH_BINARY)
+        captureBin = binary
+        capture = frame_copy
+        cv2.imshow('Binary', binary)
+        rawCapture.truncate(0)
+        if (cv2.waitKey(33)) == ord('a'):
+                break
+        
+        
+        
 
 
-cv2.imshow('Binary', captureBin)
-cv2.waitKey(0)
-lineWidth= int(np.mean([sum(255-i)/255 for i in captureBin]))
-print (lineWidth)
-with open('LineWidth.txt' , 'w') as f:
-    f.write(str(lineWidth))
+        cv2.imshow('Binary', captureBin)
+        cv2.waitKey(0)
+        lineWidth= int(np.mean([sum(255-i)/255 for i in captureBin]))
+        print (lineWidth)
+        robot.line_finder.line_width = lineWidth
+        dumpRobot(robot, fillename)
+
+if __name__ == "__main__":
+        updateLine(sys.argv[1])
+
+
     
 
