dc8ef7a761310684d632fdba1e5f618e1e536063
zack watkins <zackwatkins604@gmail.com>
2019-07-18 13:52:34 -0700

    add RoboLoader, match getLine with ROBOSOn.json

---
+++ RoboLoader.py
@@ -0,0 +1,10 @@
+#!usr/bin/bash python
+
+import json
+from collections import namedtuple
+
+def loadRobot(filename = 'ROBOSON.json'):
+    with open(filename) as f:
+        data = f.read()
+# Parse JSON into an object with attributes corresponding to dict keys.
+    return json.loads(data, object_hook=lambda d: namedtuple('Robot', d.keys())(*d.values()))
