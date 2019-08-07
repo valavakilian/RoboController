2592e335c54160804128bf1b58df2f73bb1767f3
zack watkins <zackwatkins604@gmail.com>
2019-08-07 15:35:15 -0700

    add m and t

--- MainRun.py
+++ MainRun.py
@@ -4,26 +4,49 @@ from RoboLoader import loadRobot
 from Turn import *
 from servoTest import *
 from pickupStone import *
+import sys
 
-
-# Loading Robot as an object
-# Reads variables from the file
 robot = loadRobot('ROBOSON.json')
-#changeCameraAngle(70)
-#val = Follow_Line(True, ["L","L","R", "R", "X"], [70, 70, 0, 0,0], [0.3,0.2,0.01,0.01,0.01], [0, 0.5,0.8, 1.1, 0], robot)
-#print(val)
-#changeCameraAngle(0)
-#backUp(robot, 0.3, 70)
-#Rotate(robot ,1, 60,"L")
-goToPost(robot, 150)
-pickup_stone(robot)
-print("Quitting first function")
-#curveRotate(robot, 0.1, 255, "R", 1)
-backUp(robot, 0.01,140)
-backUp(robot , 0.2, 70)
-print("Quitting backup")
-Rotate(robot, 1 , 60, "L")
-goToPost(robot, 100)
-pickup_stone(robot)
-Rotate(robot ,0.7, 60,"L")
-goStr8(robot, 4, 90)
+
+if (sys.argv[1] == '-l'):
+    changeCameraAngle(70)
+    #val = Follow_Line(True, ["L","L","R", "R", "X"], [70, 70, 0, 0,0], [0.3,0.2,0.01,0.01,0.01], [0, 0.5,0.8, 1.1, 0], robot)
+    #print(val)
+    #changeCameraAngle(0)
+    #backUp(robot, 0.3, 70)
+    #Rotate(robot ,1, 60,"L")
+    goToPost(robot, 150)
+    pickup_stone(robot)
+    print("Quitting first function")
+    #curveRotate(robot, 0.1, 255, "R", 1)
+    backUp(robot, 0.01,140)
+    backUp(robot , 0.2, 70)
+    print("Quitting backup")
+    Rotate(robot, 1 , 60, "L")
+    goToPost(robot, 100)
+    pickup_stone(robot)
+    Rotate(robot ,0.7, 60,"L")
+    goStr8(robot, 4, 90)
+
+elif (sys.argv[1] == "-r"):
+    changeCameraAngle(70)
+     # Loading Robot as an object
+    # Reads variables from the file
+    #changeCameraAngle(70)
+    val = Follow_Line(True, ["R","R","L", "L", "X"], [70, 70, 0, 0,0], [0.3,0.2,0.01,0.01,0.01], [0, 0.5,0.8, 1.1, 0], robot)
+    #print(val)
+    #changeCameraAngle(0)
+    #backUp(robot, 0.3, 70)
+    #Rotate(robot ,1, 60,"L")
+    goToPost(robot, 150)
+    pickup_stone(robot)
+    print("Quitting first function")
+    #curveRotate(robot, 0.1, 255, "R", 1)
+    backUp(robot, 0.01,140)
+    backUp(robot , 0.2, 70)
+    print("Quitting backup")
+    Rotate(robot, 1 , 60, "L")
+    goToPost(robot, 100)
+    pickup_stone(robot)
+    Rotate(robot ,0.7, 60,"L")
+    goStr8(robot, 4, 90)
